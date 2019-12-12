8050731a3d992f07c70f8f85da939a8131e132d9
ShotaTogawa <s.togawa0112@gmail.com>
2019-12-12 10:45:04 -0800

    lecture 12

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,5 +1,34 @@
-import location, { message, name, getGreeting } from "./myModules";
-import add, { substract } from "./math";
+import { GraphQLServer } from "graphql-yoga";
 
-console.log(message, name, location, getGreeting("Bob"));
-console.log(add(1, 2), substract(5, 3));
+// type definition (Schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+    location: String!
+  }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return "This is my first query";
+    },
+    name() {
+      return "Andrew";
+    },
+    location() {
+      return "Vancouver";
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => {
+  console.log("the server is up!");
+});
