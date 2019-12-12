e0507d6c1753376d3938162e66da55396dcda79e
ShotaTogawa <s.togawa0112@gmail.com>
2019-12-12 10:53:58 -0800

    lecture 12

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,25 +1,59 @@
 import { GraphQLServer } from "graphql-yoga";
 
+/**
+ * Type
+ * Scalar types - String, Boolean, Int, Float, ID(Unique Identifier)
+ */
+
 // type definition (Schema)
+// !は必須項目
 const typeDefs = `
   type Query {
-    hello: String!
+    id: ID!
     name: String!
-    location: String!
+    age: Int!
+    employed: Boolean!
+    gpa: Float
+    title: String!
+    price: Float!
+    releaseYear: Int
+    rating: Float
+    inStock: Boolean!
   }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
-    hello() {
-      return "This is my first query";
+    id() {
+      return "abc123";
     },
     name() {
       return "Andrew";
     },
-    location() {
-      return "Vancouver";
+    age() {
+      return 23;
+    },
+    employed() {
+      return true;
+    },
+    gpa() {
+      return 3.1; // nullも可能
+    },
+    title() {
+      return "udemy";
+    },
+    price() {
+      return 12.99;
+    },
+    releaseYear() {
+      return 2019;
+    },
+    rating() {
+      return null;
+    },
+    inStock() {
+      return true;
     }
   }
 };
