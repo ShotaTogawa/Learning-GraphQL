cd603f8a94b5d0f2b3bf2858f82f1c0682218312
ShotaTogawa <s.togawa0112@gmail.com>
2019-12-24 07:04:08 -0800

    finished Schemas and Queries section

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,64 +1,197 @@
 import { GraphQLServer } from "graphql-yoga";
 
-/**
- * Type
- * Scalar types - String, Boolean, Int, Float, ID(Unique Identifier)
- */
+// Scalar types - String, Boolean, Int, Float, ID
 
-// type definition (Schema)
-// !は必須項目かを決める
-// type 大文字から始まる単語
+// Demo user data
+const users = [
+  {
+    id: "1",
+    name: "Andrew",
+    email: "andrew@example.com",
+    age: 27
+  },
+  {
+    id: "2",
+    name: "Sarah",
+    email: "sarah@example.com"
+  },
+  {
+    id: "3",
+    name: "Mike",
+    email: "mike@example.com"
+  }
+];
+
+const posts = [
+  {
+    id: "10",
+    title: "GraphQL 101",
+    body: "This is how to use GraphQL...",
+    published: true,
+    author: "1",
+    comment: "1"
+  },
+  {
+    id: "11",
+    title: "GraphQL 201",
+    body: "This is an advanced GraphQL post...",
+    published: false,
+    author: "1",
+    comment: "2"
+  },
+  {
+    id: "12",
+    title: "Programming Music",
+    body: "",
+    published: false,
+    author: "2",
+    comment: "2"
+  }
+];
+
+const comments = [
+  {
+    id: "1",
+    text: "comment1",
+    author: "1",
+    post: "12"
+  },
+  {
+    id: "2",
+    text: "comment2",
+    author: "1",
+    post: "11"
+  },
+  {
+    id: "3",
+    text: "comment3",
+    author: "2",
+    post: "11"
+  },
+  {
+    id: "4",
+    text: "comment4",
+    author: "3",
+    post: "10"
+  }
+];
+
+// Type definitions (schema)
 const typeDefs = `
-  type Query {
-    greeting(name: String!, position: String!): String!
-    me: User!
-    post: Post!
-    add(a: Float!, b: Float!): Float!
-  }
+    type Query {
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
+        me: User!
+        post: Post!
+        comments: [Comment!]!
+    }
 
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    age: Int
-  }
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+        posts: [Post!]!
+        comments: [Comment!]!
+    }
 
-  type Post {
-    id: ID!
-    title: String!
-    body: String!
-    published: Boolean!
-  }
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+        author: User!
+        comments: [Comment]!
+    }
+
+    type Comment {
+        id: ID!
+        text: String!
+        author: User!
+        post: Post!
+    }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
-    greeting(parent, args, ctx, info) {
-      if (args.name) {
-        return `Hello, ${args.name}. Your position is ${args.position}`;
-      } else {
-        return "Hello";
-      }
+    comments(parent, args, ctx, info) {
+      return comments;
     },
-    add(parent, args, ctx, info) {
-      return args.a + args.b;
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
+      }
+
+      return users.filter(user => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts;
+      }
+
+      return posts.filter(post => {
+        const isTitleMatch = post.title
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+        const isBodyMatch = post.body
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+        return isTitleMatch || isBodyMatch;
+      });
     },
     me() {
       return {
-        id: "12451",
-        name: "mike",
-        email: "mike@example.com",
-        age: 28
+        id: "123098",
+        name: "Mike",
+        email: "mike@example.com"
       };
     },
     post() {
       return {
-        id: "1",
-        title: "First post",
+        id: "092",
+        title: "GraphQL 101",
         body: "",
         published: false
       };
+    }
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find(user => {
+        return user.id === parent.author;
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter(comment => {
+        return comment.post === parent.id;
+      });
+    }
+  },
+  Comment: {
+    author(parent, args, ctx, info) {
+      return users.find(user => {
+        return user.id === parent.author;
+      });
+    },
+    post(parent, arg, ctx, info) {
+      return posts.find(post => {
+        return post.id === parent.post;
+      });
+    }
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter(post => {
+        return post.author === parent.id;
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter(comment => {
+        return comment.author === parent.id;
+      });
     }
   }
 };
@@ -69,5 +202,5 @@ const server = new GraphQLServer({
 });
 
 server.start(() => {
-  console.log("the server is up!");
+  console.log("The server is up!");
 });
